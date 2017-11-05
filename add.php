9360f5414009879921666b64a1e954faa251b96b
Nerys Jimenez <jimenezpichardon785@strose.edu>
2017-11-05 15:59:23 -0500

    Add files via upload

---
+++ add.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+	$DBConnect = mysql_connect("localhost", "jimenez", "");
+	mysql_select_db("jimenez");
+	
+	$SQLstring = "SELECT MAX(PRESIDENT_ID) FROM PRESIDENTS";
+	$QueryResult = mysql_query($SQLstring);
+	
+	$Row = mysql_fetch_row($QueryResult);
+	$myID = $Row[0] + 1;	
+	
+	$name = $_POST["name"];
+	$state = $_POST["state"];
+	$year = $_POST["year"];
+	$party = $_POST["party"];
+		
+	$SQLstring = "INSERT INTO PRESIDENTS VALUES(" .
+              $myID . ",'" . $name . "','" . $state .
+              "'," . $year . ",'" . $party .
+              "');";  
+	
+	$QueryResult = mysql_query($SQLstring);
+    
+    $SQLstring2 = "SELECT * FROM PRESIDENTS;";
+	$QueryResult2 = mysql_query($SQLstring2);
+    
+    echo "<TABLE BORDER='2'>";
+	echo "<TR>";
+	for ($i = 0; $i < mysql_num_fields($QueryResult2); $i++)
+	{
+	  $field = mysql_fetch_field($QueryResult2, $i);
+	  echo "<TH> $field->name </TH>";
+	}
+	echo "</TR>";
+	$Row = mysql_fetch_row($QueryResult2);
+	do
+	{
+  		echo "<TR>";
+  		for ($i = 0; $i < mysql_num_fields($QueryResult2); $i++)
+  		{
+    		echo "<TD>{$Row[$i]}</TD>";
+  		}
+  		echo "</TR>";
+  		$Row = mysql_fetch_row($QueryResult2);
+	} while ($Row);
+   	mysql_close($DBConnect);
+	echo "</TABLE>";
+?>
+
+</body>
+</html>
